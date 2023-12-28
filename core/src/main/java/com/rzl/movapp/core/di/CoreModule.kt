eb4e665c7ed3fbc5133fddd2cd4ba66668a20c41
Rizal Rahmadani <rizalbnt121@gmail.com>
2023-12-28 19:48:50 +0800

    Menerapkan certificate pinning untuk koneksi ke server.

--- core/src/main/java/com/rzl/movapp/core/di/CoreModule.kt
+++ core/src/main/java/com/rzl/movapp/core/di/CoreModule.kt
@@ -11,6 +11,7 @@ import com.rzl.movapp.core.domain.repository.IPopularRepository
 import com.rzl.movapp.core.utils.AppExecutors
 import net.sqlcipher.database.SQLiteDatabase
 import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -52,11 +53,19 @@ val repositoryModule = module {
 val networkModule = module {
     single { AuthenticationInterceptor(apiKey = "56ea350c44428e5aa5246ad8a58067e5") }
     single {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname,"sha256/5VLcahb6x4EvvFrCF2TePZulWqrLHS2jCg9Ywv6JHog=")
+            .add(hostname,"sha256/vxRon/El5KuI4vx5ey1DgmsYmRY0nDd5Cg4GfJ8S+bg=")
+            .add(hostname,"sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+            .add(hostname,"sha256/KwccWaCgrnaw6tsrrSO61FgLacNgG2MMLq8GE6+oP5I=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(get<AuthenticationInterceptor>())
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
